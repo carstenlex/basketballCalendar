263f674b99a4a661058730a98f3f22c885ca8477
carstenlex <carsten.lex@raiffeisen.ch>
2022-02-08 13:12:53 +0100

    HU16 hinzugefügt; Ligen aktualisiert

--- src/main/java/de/carstenlex/Mannschaft.java
+++ src/main/java/de/carstenlex/Mannschaft.java
@@ -5,12 +5,12 @@ public enum Mannschaft {
     // SHortname zur Anzeige im Calendar; liga=id im Basketplan und team = id im Basketplan
     HERREN1("H1",7737,5310),
     HERREN_SEN("H2",7204,5485),
-    HU18("U18",7719,5311),
+    HU18("U18",7975,5311),
     HU12("U12",7729,5312),
     HU14("U14",7727,5369),
-    //HU15("U16",7217,5488),
+    HU16("U16",7960,5488),
     //HU09("U10",7236,5313),
-    MIX("Mix",7745,5489);
+    MIX("Mix",7984,5489);
 
 
     private String shortName;
